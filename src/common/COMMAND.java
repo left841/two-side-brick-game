9ef9773bed0009a1d86519486e4497d4f398df28
Savkin Yuriy <catasd@yandex.ru>
2020-10-19 22:09:13 +0300

    starting working with server model

---
+++ src/common/COMMAND.java
@@ -0,0 +1,11 @@
+package common;
+
+public enum COMMAND {
+    CONNECT,
+    DISCONNECT,
+    READY,
+    LEFT,
+    UP,
+    RIGHT,
+    DOWN
+}
--- src/server/model/ModelServer.java
+++ src/server/model/ModelServer.java
@@ -1,5 +1,62 @@
 package server.model;
 
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+import common.*;
+
+import static java.lang.Thread.sleep;
+
 class ModelServer implements IModelServer {
+
+    public class Session {
+
+        private Queue<COMMAND> player1q;
+        private Queue<COMMAND> player2q;
+
+        private void mainLoop() {
+            byte[][] gameField = new byte[30][30];
+            byte frame_cycle_num = 0;
+
+            while (true) {
+
+
+                try {
+                    sleep(200);
+                }
+                catch (InterruptedException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+
+        }
+
+
+
+
+
+        public Session() {
+            player1q = new ConcurrentLinkedQueue<COMMAND>();
+            player2q = new ConcurrentLinkedQueue<COMMAND>();
+
+            Thread sessionThread = new Thread() {
+                @Override
+                public void run() {
+                    mainLoop();
+                }
+            };
+            sessionThread.start();
+        }
+
+        public void addPlayer1Cmd(COMMAND cmd) {
+            player1q.add(cmd);
+        }
+
+        public void addPlayer2Cmd(COMMAND cmd) {
+            player2q.add(cmd);
+        }
+    }
 
 }
